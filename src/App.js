af5c6e64b8c43680ce33f20dff944e330b9318fc
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-04 17:07:26 -0500

    less OOOO

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>WOHOOOOOOOOOOO</h1>
+        <h1>WOHOO</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
