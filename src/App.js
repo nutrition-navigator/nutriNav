f36045a18bb35dde1ce619bfe2d12f7697f95f14
Haley Morris <haleymor@smartt.email>
2020-03-05 22:17:38 -0500

    created Home.js and toggle button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Nav from './Nav';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        < Nav />
-        <h1>App is running</h1>
+        <Home />
       </div>
     );
   }
