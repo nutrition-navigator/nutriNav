b0216a55e19fba2a5c6b4b8eee4d8ca3f074cacb
Haley Morris <haleymor@smartt.email>
2020-03-05 21:21:59 -0500

    basic nav styling

--- src/Nav.js
+++ src/Nav.js
@@ -5,16 +5,15 @@ import { faExchangeAlt, faHome, faHeart, faSearch } from '@fortawesome/free-soli
 class Nav extends Component {
   render() {
     return (
-      <div>
+      <div className="navigation">
         <nav>
-          <ul>
-            <li className="navIcon"><FontAwesomeIcon icon={faHome} /></li>
-            <li className="navIcon"><FontAwesomeIcon icon={faHeart} /></li>
-            <li>LOGO</li>
-            <li className="navIcon"><FontAwesomeIcon icon={faSearch} /></li>
-            <li className="navIcon"><FontAwesomeIcon icon={faExchangeAlt} /></li>
+          <ul className="nav-bar">
+            <li className="nav-icon nav"><FontAwesomeIcon icon={faHome} /></li>
+            <li className="nav-icon nav"><FontAwesomeIcon icon={faHeart} /></li>
+            <li className="logo-icon nav">LOGO</li>
+            <li className="nav-icon nav"><FontAwesomeIcon icon={faSearch} /></li>
+            <li className="nav-icon nav"><FontAwesomeIcon icon={faExchangeAlt} /></li>
           </ul>
-
         </nav>
       </div>
     )
