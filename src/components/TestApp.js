45269b23a872e3b8b48b7cbbef03c14852980f60
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-06 15:42:12 -0500

    redo

--- src/components/TestApp.js
+++ src/components/TestApp.js
@@ -7,81 +7,86 @@ class TestApp extends Component {
     super();
     this.state = {
       targetNutrients: [
-        { name: "Vitamin A", unit: "IU" },
-        { name: "Vitamin D", unit: "IU" },
-        { name: "Vitamin B-6", unit: "mg" },
-        { name: "Vitamin C", unit: "mg" },
-        { name: "Vitamin E", unit: "mg" },
-        { name: "Magnesium", unit: "mg" },
-        { name: "Zinc", unit: "mg" },
-        { name: "Iron", unit: "mg" },
-        { name: "Fiber", unit: "g" }
+        {name: "Vitamin A", unit: "IU"},
+        {name: "Vitamin D", unit: "IU"},
+        {name: "Vitamin B-6", unit: "mg"},
+        {name: "Vitamin C", unit: "mg"},
+        {name: "Vitamin E", unit: "mg"},
+        {name: "Magnesium", unit: "mg"},
+        {name: "Zinc", unit: "mg"},
+        {name:"Iron", unit: "mg"},
+        {name: "Fiber", unit: "g"}
       ],
+      showIDs: false,
       type: "common",
-      search: "burger", 
+      search: "cheese", // from input
+      food: {},
+      showValues: false,
       nutrientsValues: [],
       nutrients: [
-        { name: "Vitamin A", id: 318 },
-        { name: "Vitamin D", id: 324 },
-        { name: "Vitamin B-6", id: 415 },
-        { name: "Vitamin C", id: 401 },
-        { name: "Vitamin E", id: 323 },
-        { name: "Magnesium", id: 304 },
-        { name: "Zinc", id: 309 },
-        { name: "Iron", id: 303 },
-        { name: "Fiber", id: 291 }
+        { name: 'Vitamin A', id: 318 },
+        { name: 'Vitamin D', id: 324 },
+        { name: 'Vitamin B-6', id: 415 },
+        { name: 'Vitamin C', id: 401 },
+        { name: 'Vitamin E', id: 323},
+        { name: 'Magnesium', id: 304},
+        { name: 'Zinc', id: 309 },
+        { name: 'Iron', id: 303 },
+        { name: 'Fiber', id: 291 }
       ]
     };
   }
 
-  getNutrients = () => {
-    let nutrientsAPI = [];
-    axios({
-      url: "https://trackapi.nutritionix.com/v2/utils/nutrients",
-      method: "GET",
-      responseType: "json",
-      headers: {
-        "x-app-id": "f55663ad",
-        "x-app-key": "8a4711b9498f267927ad120c76ab8808",
-        "x-remote-user-id": "0"
-      }
-    }).then(response => {
-      nutrientsAPI = response.data;
-      const tempNutrients = this.state.targetNutrients.map(nutrient => {
-        return {
-          name: nutrient.name,
-          id: this.getNutrientID(nutrient.name, nutrientsAPI),
-          unit: nutrient.unit
-        };
-      });
-      this.setState(
-        {
-          nutrients: tempNutrients
-        },
-        () => {}
-      );
-    });
-  };
+  // getNutrients = () => {
+  //   let nutrientsAPI = [];
+  //   axios({
+  //     url: "https://trackapi.nutritionix.com/v2/utils/nutrients",
+  //     method: "GET",
+  //     responseType: "json",
+  //     headers: {
+  //       "x-app-id": "f55663ad",
+  //       "x-app-key": "8a4711b9498f267927ad120c76ab8808",
+  //       "x-remote-user-id": "0"
+  //     }
+  //   }).then(response => {
+  //     nutrientsAPI = response.data;
+  //     const tempNutrients = this.state.targetNutrients.map(nutrientName => {
+  //       return {
+  //         name: nutrientName,
+  //         id: this.getNutrientID(nutrientName, nutrientsAPI)
+  //       };
+  //     });
+  //     this.setState(
+  //       {
+  //         nutrients: tempNutrients
+  //       },
+  //       () => {
+  //       }
+  //     );
+  //   });
+  // };
 
-  getNutrientID = (name, nutrientsAPI) => {
-    const tempNutrient = nutrientsAPI.filter(nutrient => {
-      return nutrient.usda_nutr_desc.includes(name);
-    });
-    return tempNutrient[0].attr_id;
-  };
+  // getNutrientID = (name, nutrientsAPI) => {
+  //   const tempNutrient = nutrientsAPI.filter(nutrient => {
+  //     return nutrient.usda_nutr_desc.includes(name);
+  //   });
+  //   return tempNutrient[0].attr_id;
+  // };
 
   getValue = (id, foodNutrients) => {
+    console.log('GETVALUE ID PASSED', id);
     const tempNutrient = foodNutrients.filter(nutrient => {
       return nutrient.attr_id === id;
     });
-    return tempNutrient.length > 0 ? tempNutrient[0].value : 0;
+    console.log('NUTRIENT[0]VALUE', tempNutrient);
+    return (tempNutrient.length > 0 )? (tempNutrient[0].value) : 0;
   };
 
   getDetails = (id, type) => {
     const urlEndpoint = type === "common" ? "natural/nutrients" : "search/item";
     const method = type === "common" ? "POST" : "GET";
-    const params = type === "common" ? {} : { nix_item_id: id };
-    const data = type === "common" ? { query: id } : {};
+    const params = type === "common" ? {} : {nix_item_id: id};
+    const data = type === "common" ? {query: id} : {};
     return axios({
       url: `https://trackapi.nutritionix.com/v2/${urlEndpoint}`,
       method: method,
@@ -93,23 +98,20 @@ class TestApp extends Component {
       },
       data: data,
       params: params
-    });
-  };
+    })
+  }
+     
+  
+        
 
   componentDidMount() {
-    this.getNutrients();
     this.getDetails(this.state.search, this.state.type);
   }
 
   render() {
     return (
       <div>
-        <FoodDetail
-          getDetails={this.getDetails}
-          getValue={this.getValue}
-          nutrients={this.state.nutrients}
-          type={this.state.type}
-        ></FoodDetail>
+        <FoodDetail getDetails={this.getDetails} getValue={this.getValue}></FoodDetail>
       </div>
     );
   }
--- src/pages/FoodDetail.js
+++ src/pages/FoodDetail.js
@@ -8,7 +8,19 @@ class FoodDetail extends Component {
       food: {},
       nutrientValues: [],
       props: {
-        match: { params: { id: "burger" } }
+        type: "brand",
+        nutrients: [
+          { name: "Vitamin A", id: 318, unit: "IU" },
+          { name: "Vitamin D", id: 324, unit: "IU" },
+          { name: "Vitamin B-6", id: 415, unit: "mg" },
+          { name: "Vitamin C", id: 401, unit: "mg" },
+          { name: "Vitamin E", id: 323, unit: "mg" },
+          { name: "Magnesium", id: 304, unit: "mg" },
+          { name: "Zinc", id: 309, unit: "mg" },
+					{ name: "Iron", id: 303, unit: "mg" },
+					{ name: "Fiber", id: 303, unit: "g" }
+        ],
+        match: { params: { id: "54836a2305e256f87e091b04" } }
       }
     };
 	}
@@ -18,7 +30,7 @@ class FoodDetail extends Component {
 
   componentDidMount() {
 		this.props
-      .getDetails(this.state.props.match.params.id, this.props.type)
+      .getDetails(this.state.props.match.params.id, this.state.props.type)
       .then(response => {
 				let values = [];
         const food = response.data.foods[0];
@@ -27,19 +39,21 @@ class FoodDetail extends Component {
             food
           },
           () => {
-						// console.log('FOOD', this.state.food);
-						// console.log('FOOD FULL NUTRIENTS', this.state.food.full_nutrients);
+						console.log('FOOD', this.state.food);
+						console.log('FOOD FULL NUTRIENTS', this.state.food.full_nutrients);
           }
         );
-        values = this.props.nutrients.map(nutrient => {
+        values = this.state.props.nutrients.map(nutrient => {
+					console.log('NUTRIENT ID', nutrient.id);
 					const value = this.props.getValue(nutrient.id, food.full_nutrients);
+					console.log('RETURNING VALUE', value);
           return {
             name: nutrient.name,
             id: nutrient.id,
-						value: value,
-						unit: nutrient.unit,
+            value: value
           };
         });
+				console.log('VALUES ARRAY', values);
         this.setState(
           {
             nutrientValues: values
