c5e9fcf084a9286c9dc7266bf8411142646a28f7
Haley Morris <haleymor@smartt.email>
2020-03-06 11:09:03 -0500

    merged home.js and moved nav.js to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './Home';
+import Home from './pages/Home';
 import './App.css';
 import TestApp from './components/TestApp';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import Nav from '../components/Nav';
 
 class Home extends Component {
-	state = {};
 	render() {
-		return <h1>Home</h1>;
+		return (
+			<div className="home-page">
+				<Nav />
+				<input type="search" name="search" id="search" />
+				<div className="toggle-switch">
+					<input
+						type="checkbox"
+						className="toggle-switch-checkbox"
+						name="toggleSwitch"
+						id="toggleSwitch"
+					/>
+					<label className="toggle-switch-label" htmlFor="toggleSwitch">
+						<span className="toggle-switch-inner" />
+						<span className="toggle-switch-switch" />
+					</label>
+				</div>
+
+			</div>
+		)
 	}
 }
 
