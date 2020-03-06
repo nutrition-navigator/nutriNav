e0e4f8bdc0670caf8c9abcfb26d7a6abc8a2ac7c
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-06 15:24:21 -0500

    details from parent

--- src/pages/FoodDetail.js
+++ src/pages/FoodDetail.js
@@ -1,10 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import Nav from "../components/Nav";
 
 class FoodDetail extends Component {
-	state = {};
-	render() {
-		return <h1>FoodDetail</h1>;
+  constructor() {
+    super();
+    this.state = {
+      food: {},
+      nutrientValues: [],
+      props: {
+        match: { params: { id: "burger" } }
+      }
+    };
 	}
+	
+	// 54836a2305e256f87e091b04
+
+
+  componentDidMount() {
+		this.props
+      .getDetails(this.state.props.match.params.id, this.props.type)
+      .then(response => {
+				let values = [];
+        const food = response.data.foods[0];
+        this.setState(
+          {
+            food
+          },
+          () => {
+						// console.log('FOOD', this.state.food);
+						// console.log('FOOD FULL NUTRIENTS', this.state.food.full_nutrients);
+          }
+        );
+        values = this.props.nutrients.map(nutrient => {
+					const value = this.props.getValue(nutrient.id, food.full_nutrients);
+          return {
+            name: nutrient.name,
+            id: nutrient.id,
+						value: value,
+						unit: nutrient.unit,
+          };
+        });
+        this.setState(
+          {
+            nutrientValues: values
+          },
+          () => {
+            console.log('STATE VALUES ARRAY', this.state.nutrientValues);
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  render() {
+		return (
+      <Fragment>
+        <Nav />
+				<h1> Food Details </h1>
+				<button>Add To Favorites</button>
+      </Fragment>
+    );
+		
+  }
 }
 
 export default FoodDetail;
