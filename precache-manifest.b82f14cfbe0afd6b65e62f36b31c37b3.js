47ba8924dcc579268d94fb62d3f7cf50846adcbf
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-09 17:05:29 -0400

    Updates

---
+++ precache-manifest.b82f14cfbe0afd6b65e62f36b31c37b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab7b8aba90943db7f60dc4e518c79e1",
+    "url": "/nutriNav/index.html"
+  },
+  {
+    "revision": "cbb2c52e964ef39b1fd7",
+    "url": "/nutriNav/static/css/main.15193a86.chunk.css"
+  },
+  {
+    "revision": "8ce59c6e660fdf5b9e2a",
+    "url": "/nutriNav/static/js/2.caf16494.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/nutriNav/static/js/2.caf16494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb2c52e964ef39b1fd7",
+    "url": "/nutriNav/static/js/main.f1308f3c.chunk.js"
+  },
+  {
+    "revision": "48ae16bbc6b7b314b68f",
+    "url": "/nutriNav/static/js/runtime-main.b00aee75.js"
+  },
+  {
+    "revision": "ba2638de246d5c27ede21001d330d327",
+    "url": "/nutriNav/static/media/nutriNavLogo.ba2638de.svg"
+  }
+]);
