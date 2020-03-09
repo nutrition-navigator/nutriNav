95e00fd6eacb9684e5790a502c3d692740ac768b
Aron Tolentino <arontolentino@outlook.com>
2020-03-09 14:52:38 -0400

    Style foodDetail

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -3,25 +3,29 @@ import Nav from '../components/Nav';
 import ComparisonCard from '../components/ComparisonCard';
 
 class Compare extends Component {
-	
-
 	// componentDidMount() {
 	// 	console.log('compare array:', this.state.foodsToCompare);
 	// }
-	
 
 	render() {
 		return (
-      <div className="comparisonPage">
-        <Nav />
-        <h1>Compare Items</h1>
-        <div className="comparisonContainer">
-          {this.props.userCompared.map(food => {
-            return <ComparisonCard key={food.key} id={food.key} food={food} removeItem={this.props.removeItem} />;
-          })}
-        </div>
-      </div>
-    );
+			<div className="comparisonPage">
+				<Nav />
+				<h1 className="pageTitle">Compare Items</h1>
+				<div className="comparisonContainer">
+					{this.props.userCompared.map(food => {
+						return (
+							<ComparisonCard
+								key={food.key}
+								id={food.key}
+								food={food}
+								removeItem={this.props.removeItem}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		);
 	}
 }
 
