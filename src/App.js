99acec07e3bbd0cc768f835827211c0f603e9a24
Ania-M-Pienio <aniapienio@gmail.com>
2020-03-09 19:41:52 -0400

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 import Favourites from './pages/Favourites';
 import FoodDetail from './pages/FoodDetail';
