9701825e437ea3a2ca1d95c0e079b842a2a6177e
Aron Tolentino <arontolentino@outlook.com>
2020-03-10 14:50:18 -0400

    Restyle toaster

--- src/components/Toaster.js
+++ src/components/Toaster.js
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Toaster extends Component {
-  constructor() {
-    super();
-    this.state = {
-      theme: {
-        "SUCCESS" : "success",
-        "SAVE FAILED": "failed",
-      }
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			theme: {
+				SUCCESS: 'success',
+				'SAVE FAILED': 'failed'
+			}
+		};
+	}
 
-  render() {
-    const theme = this.state.theme[this.props.overall];
-    return (
-      <div
-        className="hello"
-      >
-        <div className={"toaster " + theme}>
-          <h1>{this.props.overall}</h1>
-          <p> {this.props.message}</p>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const theme = this.state.theme[this.props.overall];
+		return (
+			<div className="hello">
+				<div className={'toaster ' + theme}>
+					<h2>{this.props.overall}</h2>
+					<p> {this.props.message}</p>
+				</div>
+			</div>
+		);
+	}
 }
 export default Toaster;
