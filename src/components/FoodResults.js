7b4d2d5b12b8fde5eb955fbd2ad209030fc716a1
Aron Tolentino <arontolentino@outlook.com>
2020-03-10 14:40:23 -0400

    Add image to no results message

--- src/components/FoodResults.js
+++ src/components/FoodResults.js
@@ -7,11 +7,18 @@ class FoodResults extends Component {
 	render() {
 		return (
 			<div className="foodContainer">
-				{this.props.foodItems.length===0 ? <p>Your search found nothing, please try something else.</p> :
+				{this.props.foodItems.length === 0 ? (
+					<div className="noResults">
+						<img src="https://i.imgur.com/S81lkaK.png" alt="" />
+						<p>
+							Sorry! We didn't find anything. Please try another search term.
+						</p>
+					</div>
+				) : (
 					this.props.foodItems.map((foodItem, index) => (
 						<FoodCard key={index} foodItem={foodItem} />
-					))}
-				
+					))
+				)}
 			</div>
 		);
 	}
